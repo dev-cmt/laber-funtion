1fd94f2822cc0a3ac25b29f60d24ad7dc321e5de
Motiur Rahman <112879061+dev-cmt@users.noreply.github.com>
2026-01-22 19:26:17 +0600

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,4 +32,99 @@ class HomeController extends Controller
         return view('frontend.welcome', compact('seotags','breadcrumbs'));
     }
 
+    public function shop()
+    {
+        // SEO
+        $page = Page::with('seo')->where('slug','home')->firstOrFail();
+        $this->setSeo([
+            'title'       => $page->seo->meta_title ?? $page->title,
+            'description' => $page->seo->meta_description ?? '',
+            'keywords'    => $this->formatKeywords($page->seo->meta_keywords ?? ''),
+            'image'       => $page->seo->og_image ?? '',
+            'canonical'   => url()->current(),
+        ]);
+        $seotags = $this->generateTags();
+
+        $breadcrumbs = $this->generateBreadcrumbJsonLd([
+            ['name' => 'Home', 'url' => url('/')],
+        ]);
+        return view('frontend.shop', compact('seotags','breadcrumbs'));
+    }
+
+    public function productDetails()
+    {
+        // SEO
+        $page = Page::with('seo')->where('slug','home')->firstOrFail();
+        $this->setSeo([
+            'title'       => $page->seo->meta_title ?? $page->title,
+            'description' => $page->seo->meta_description ?? '',
+            'keywords'    => $this->formatKeywords($page->seo->meta_keywords ?? ''),
+            'image'       => $page->seo->og_image ?? '',
+            'canonical'   => url()->current(),
+        ]);
+        $seotags = $this->generateTags();
+
+        $breadcrumbs = $this->generateBreadcrumbJsonLd([
+            ['name' => 'Home', 'url' => url('/')],
+        ]);
+        return view('frontend.product-details', compact('seotags','breadcrumbs'));
+    }
+
+    public function checkout()
+    {
+        // SEO
+        $page = Page::with('seo')->where('slug','home')->firstOrFail();
+        $this->setSeo([
+            'title'       => $page->seo->meta_title ?? $page->title,
+            'description' => $page->seo->meta_description ?? '',
+            'keywords'    => $this->formatKeywords($page->seo->meta_keywords ?? ''),
+            'image'       => $page->seo->og_image ?? '',
+            'canonical'   => url()->current(),
+        ]);
+        $seotags = $this->generateTags();
+
+        $breadcrumbs = $this->generateBreadcrumbJsonLd([
+            ['name' => 'Home', 'url' => url('/')],
+        ]);
+        return view('frontend.checkout', compact('seotags','breadcrumbs'));
+    }
+
+    public function cart()
+    {
+        // SEO
+        $page = Page::with('seo')->where('slug','home')->firstOrFail();
+        $this->setSeo([
+            'title'       => $page->seo->meta_title ?? $page->title,
+            'description' => $page->seo->meta_description ?? '',
+            'keywords'    => $this->formatKeywords($page->seo->meta_keywords ?? ''),
+            'image'       => $page->seo->og_image ?? '',
+            'canonical'   => url()->current(),
+        ]);
+        $seotags = $this->generateTags();
+
+        $breadcrumbs = $this->generateBreadcrumbJsonLd([
+            ['name' => 'Home', 'url' => url('/')],
+        ]);
+        return view('frontend.cart', compact('seotags','breadcrumbs'));
+    }
+
+    public function wishlist()
+    {
+        // SEO
+        $page = Page::with('seo')->where('slug','home')->firstOrFail();
+        $this->setSeo([
+            'title'       => $page->seo->meta_title ?? $page->title,
+            'description' => $page->seo->meta_description ?? '',
+            'keywords'    => $this->formatKeywords($page->seo->meta_keywords ?? ''),
+            'image'       => $page->seo->og_image ?? '',
+            'canonical'   => url()->current(),
+        ]);
+        $seotags = $this->generateTags();
+
+        $breadcrumbs = $this->generateBreadcrumbJsonLd([
+            ['name' => 'Home', 'url' => url('/')],
+        ]);
+        return view('frontend.wishlist', compact('seotags','breadcrumbs'));
+    }
+
 }
